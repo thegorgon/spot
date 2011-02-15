d17894434d0c40d55b50d37d89b4de5fa8d2e224
Jesse Reiss <jreiss@oogalabs.com>
2011-02-15 10:03:15 -0800

    file cleanup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 redis_config = YAML.load_file(File.join(Rails.root, "config", "redis.yml"))[Rails.env] || {}
-session_config = {:expire_after => 30.days, :domain => :all, :key => "_spot_session"}.merge!(redis_config.symbolize_keys!)
-
-puts session_config.inspect
+session_config = {:expire_after => 30.days, :domain => :all, :key => "_spot_session"}
+session_config.merge!(redis_config.symbolize_keys!)
 
 Spot::Application.config.session_store :redis_session_store, session_config
