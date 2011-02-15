e60bcdb4bfc5fdc6e929b4f3ee48500701f0964f
Jesse Reiss <jreiss@oogalabs.com>
2011-02-15 13:01:07 -0800

    deploy with sphinx complete

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,18 +10,22 @@ set :branch,        "master"
 set :deploy_via,    :remote_cache
 set :deploy_to,     '/u/app'
 set :keep_releases, 10
-set :num_servers,   1
+set :app_server_n,  1
+set :bg_server_n,   1
 set :rails_env,     'production'
 
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
-app_servers = (1..num_servers).reject { |i| i == -1 }.collect { |num| "spot#{num}.ec2" }
+app_servers = (1..app_server_n).reject { |i| i == -1 }.collect { |num| "spot#{num}.ec2" }
 app_servers.each do |server|
   role :web, server
   role :app, server
 end
 role :db, app_servers.first, :primary => true
+
+bg_servers = (1..bg_server_n).reject { |i| i == -1 }.collect { |num| "spotbg#{num}.ec2" }
+bg_servers.each { |server| role :bg, server }
 
 # Passenger Deploy Settings
 namespace :deploy do
@@ -30,6 +34,21 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
     # run "curl -s http://www.spot-app.com $2 > /dev/null"
+  end
+  namespace :sphinx do
+    desc "Stop sphinx"
+    task :stop, :roles => :bg do
+      run "cd #{current_path} && sudo rake thinking_sphinx:stop RAILS_ENV=#{rails_env}"
+    end
+    desc "Start sphinx"
+    task :start, :roles => :bg do
+      run "cd #{current_path} && sudo rake thinking_sphinx:configure RAILS_ENV=#{rails_env} && sudo rake thinking_sphinx:start RAILS_ENV=#{rails_env}"
+    end
+    desc "Restart sphinx"
+    task :restart, :roles => :bg do
+      stop
+      start
+    end
   end
 end
 
@@ -45,3 +64,4 @@ after 'deploy' do
   system("git tag release-`date +%Y_%m_%d-%H%M`")
   system("git push origin master --tags")
 end
+after 'deploy', 'deploy:sphinx:restart'
