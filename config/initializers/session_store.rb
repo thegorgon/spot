5a0799be3f481f37faa1c79df1db29d211853cbf
Jesse Reiss <jreiss@oogalabs.com>
2011-02-15 09:23:43 -0800

    redis fix part 4, symbolizing keys appears to work

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
 redis_config = YAML.load_file(File.join(Rails.root, "config", "redis.yml"))[Rails.env] || {}
-session_config = {:expire_after => 30.days, :domain => :all, :key => "_spot_session"}.merge!(redis_config)
+session_config = {:expire_after => 30.days, :domain => :all, :key => "_spot_session"}.merge!(redis_config.symbolize_keys!)
 
-if redis_config.present?
-  # Configure session
-  Spot::Application.config.session_store :redis_session_store, session_config
-else
-  Spot::Application.config.session_store :cookie_store, session_config
-end
+puts session_config.inspect
+
+Spot::Application.config.session_store :redis_session_store, session_config
