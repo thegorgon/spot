eda25d6a3275eca099ed9dde1d50a27ed05dca16
Jesse Reiss <jreiss@oogalabs.com>
2011-03-07 18:27:26 -0800

    separation of api and db

--- app/models/google_place.rb
+++ app/models/google_place.rb
@@ -6,13 +6,10 @@ class GooglePlace < ActiveRecord::Base
   
   # Accepts any normalizeable LatLng params (e.g. lat and lng, ll, origin)
   # GooglePlace.search(:query => "query", :radius => accuracy, :lat => Lat, :lng => Lng, :page => 2, :exclude => "Daves")
-  def self.search(*args)
-    request = Curl::Easy.perform(search_url(*args))
-    json = JSON.parse(request.body_str) rescue nil
-    if json && json["responseData"] && json["responseData"]["results"]
-      results = json["responseData"]["results"]
-      results.map! { |r| parse(r) }
-      results.compact!
+  def self.search(params={})
+    results = Google::Place.search(params)
+    if results.present?
+      results.map! { |gp| GooglePlace.from_google(gp) }
       results = results.hash_by { |gp| gp.cid }
       saved = where(:cid => results.keys).includes(:place).all.hash_by { |gp| gp.cid }
       results.keys.each do |cid|
@@ -20,48 +17,20 @@ class GooglePlace < ActiveRecord::Base
       end
       results.values
     else
-      raise ExternalServiceError, "Invalid results from google place search"
+      []
     end
   end
   
-  def self.search_url(*args)
-    origin = Geo::LatLng.normalize(*args)
-    raise ArgumentError, "Invalid GooglePlace search: Please provide a normalizeable LatLng in your arguments" unless origin
-    options = args.extract_options!
-    page =  options[:page].blank?? 1 : options[:page]
-    start = 8 * (page - 1)
-    if !options[:query].blank?
-      query = options[:query]
-    elsif options[:exclude] && options[:query]
-      query = "#{options[:query]} #{options[:exclude].collect{ |place| '-"' + place.name + '"' }.join(" ")}"
-    else
-      query = "*"
+  def self.from_google(gp)
+    object = new
+    columns.each do |col|
+      if gp.respond_to?(col.name)
+        object.send("#{col.name}=", gp.send(col.name))
+      end
     end
-    url = "http://ajax.googleapis.com/ajax/services/search/local?v=1.0&q=#{CGI.escape(query)}&rsz=large&start=#{start}&sll=#{origin.ll}"
-    Rails.logger.info("google-search: #{url}")
-    url
+    object
   end
-    
-  def self.parse(result)
-    place = new
-    if result && result.kind_of?(Hash) && result['url'].present? && (cid = result['url'].gsub(/.*cid=(.*)$/, '\1')) && cid.to_i > 0
-      place.cid = cid.to_i.to_s
-      place.name = result['titleNoFormatting']
-      place.street_address = result['streetAddress'] 
-      place.listing_type = result['listingType']
-      place.city = result['city']
-      place.region = result['region']
-      place.country = result['country']
-      place.address = result['addressLines'] && result['addressLines'].join("\n")
-      place.phone_number = result['phoneNumbers'] && result['phoneNumbers'].first.kind_of?(Hash) ? result['phoneNumbers'].first['number'] : nil
-      place.lat = result['lat'].to_f
-      place.lng = result['lng'].to_f
-      place
-    else
-      nil
-    end
-  end
-      
+  
   def bind_to_place!
     if place_id && place
       place.canonical
---
+++ vendor/plugins/google/lib/google/place.rb
@@ -0,0 +1,77 @@
+module Google
+  class Place
+    ENDPOINT = "http://ajax.googleapis.com/ajax/services/search/local"
+    REFERRER = Rails.env.production?? "www.spot-app.com" : "www.rails.local:3000"
+    attr_accessor :cit, :name, :street_address, :listing_type, :city, :region, :country, :address, :phone_number, :lat, :lng
+    attr_accessor :cid
+    
+    def self.search(params={})
+      json = json(params)
+      status = json["responseStatus"].to_i rescue -1
+      parsed = []
+      if status == 200
+        results = json["responseData"]["results"]
+        parsed = results.map { |r| parse(r) }
+        parsed.compact!
+        parsed
+      end
+    end
+    
+    def self.parse(json)
+      if json && json.kind_of?(Hash) && json['url'].present? && (cid = json['url'].gsub(/.*cid=(.*)$/, '\1')) && cid.to_i > 0
+        object = new
+        object.cid = cid.to_i.to_s
+        object.name = json['titleNoFormatting']
+        object.street_address = json['streetAddress'] 
+        object.listing_type = json['listingType']
+        object.city = json['city']
+        object.region = json['region']
+        object.country = json['country']
+        object.address = json['addressLines'] && json['addressLines'].join("\n")
+        object.phone_number = json['phoneNumbers'] && json['phoneNumbers'].first.kind_of?(Hash) ? json['phoneNumbers'].first['number'] : nil
+        object.lat = json['lat'].to_f
+        object.lng = json['lng'].to_f
+        object
+      else
+        nil
+      end
+    end
+    
+    def self.json(params={})
+      JSON.parse(raw(params)) rescue nil
+    end
+    
+    def self.raw(params={})
+      fetch_url = url(params)
+      Rails.logger.info "[Google Photo] Performing GET request to url : #{fetch_url}"
+      curl = Curl::Easy.http_get(fetch_url) do |curl|
+        curl.headers["Referer"] = "http://#{REFERRER}"
+      end
+      curl.body_str
+    end
+    
+    def self.url(options={})
+      origin = Geo::LatLng.normalize(options)
+      raise ArgumentError, "Invalid GooglePlace search: Please provide a normalizeable LatLng in your arguments" unless origin
+      page =  options[:page].blank?? 1 : options[:page]
+      start = 8 * (page - 1)
+      query = options[:q] || options[:query]
+      if options[:exclude] && query.present?
+        query = "#{options[:q]} #{options[:exclude].collect{ |place| '-"' + place.name + '"' }.join(" ")}"
+      elsif query.blank?
+        query = "*"
+      end
+      url = "#{ENDPOINT}"
+      params = {}
+      params[:v] ||= "1.0"
+      params[:q] = CGI.escape(query)
+      params[:rsz] = :large
+      params[:start] = start
+      params[:sll] = origin.ll
+      url += "?#{params.to_query}" if params.keys.length > 0
+      Rails.logger.info("google-search: #{url}")
+      url
+    end
+    
+  end
+end
