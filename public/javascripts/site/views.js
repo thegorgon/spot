9c4a9f1a198166ca489ad66b80a076350dc9a0fb
Jesse Reiss <jreiss@oogalabs.com>
2011-03-24 22:49:58 -0700

    noisy bg less noisy

--- public/javascripts/site/views.js
+++ public/javascripts/site/views.js
@@ -46,9 +46,9 @@
     },
     site_previews_index: function() {
       var slideshow = $("#slideshow").slideshow({
-        slides: [{ src : '/images/assets/slideshow/slide_01.jpg', gravity: '1.0x1.0' },
-                 { src : '/images/assets/slideshow/slide_02.jpg', gravity: '1.0x1.0' },
-                 { src : '/images/assets/slideshow/slide_03.jpg', gravity: '1.0x-0.5' }],
+        slides: [{ src : '/images/assets/slideshow/slide_01.jpg', gravity: '0.5x0.5' },
+                 { src : '/images/assets/slideshow/slide_02.jpg', gravity: '0.5x0.5' },
+                 { src : '/images/assets/slideshow/slide_03.jpg', gravity: '0.5x0.5' }],
         start: new Date().getSeconds() % 3
       });
       $('#nextslide').click(function(e) {  
