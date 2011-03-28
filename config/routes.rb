0d4dfc83314e1039834b30f0f4c2568281afd8ee
Jesse Reiss <jreiss@oogalabs.com>
2011-03-27 19:01:59 -0700

    remove session test

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,6 @@ Spot::Application.routes.draw do
         put "ignore"
       end
     end
-    resource :session_test, :only => [:new, :show], :controller => "session_test"
     resource :search, :only => [:new, :show], :controller => "search"
     root :to => "home#index"
   end
