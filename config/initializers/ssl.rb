ba8ed46ae6cb72e5f2f8420bd1e8828f27e55f8c
Jesse Reiss <jreiss@oogalabs.com>
2011-04-09 15:39:38 -0700

    ssl fix

--- config/initializers/ssl.rb
+++ config/initializers/ssl.rb
@@ -1,3 +1,3 @@
 if Rails.env.production?  
-  Rails.configuration.middleware.use Rack::SslEnforcer, :except => [/^\/places\/.+/, /^\/previews\/\d+\/share\//], :strict => true
+  Rails.configuration.middleware.use Rack::SslEnforcer, :except => [/^\/places\/.+/, /^\/previews\/\d+\/share/], :strict => true
 end
