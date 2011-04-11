b8829b3a4d0320c7ef5290a0b86c49b67255b8fc
Jesse Reiss <jreiss@oogalabs.com>
2011-04-11 13:42:07 -0700

    take all the details away, just require ssl

--- config/initializers/ssl.rb
+++ config/initializers/ssl.rb
@@ -1,4 +1,3 @@
 if Rails.env.production?  
-  Rails.configuration.middleware.use Rack::SslEnforcer, :only_hosts => /api\d*\.spot\-app\.com/
-  # Rails.configuration.middleware.use Rack::SslEnforcer, :except => [/^\/places\//, /^\/previews\/\d+\/share/], :strict => true, :except_hosts => /api\d*\.spot\-app\.com/
+  Rails.configuration.middleware.use Rack::SslEnforcer
 end
