f969eb8a94703533da3f5200b25453a088d915d4
Jesse Reiss <jreiss@oogalabs.com>
2011-04-11 15:40:02 -0700

    actually need to sweep

--- app/sweepers/place_sweeper.rb
+++ app/sweepers/place_sweeper.rb
@@ -2,17 +2,19 @@ class PlaceSweeper < ActionController::Caching::Sweeper
   observe Place
 
   def after_update(place)
+    Rails.logger.info "place-sweeper: after_update place #{place.to_param}"
     expire_cache_for(place)
   end
   
   def after_create(place)
+    Rails.logger.info "place-sweeper: after_create place #{place.to_param}"
     expire_cache_for(place)
   end
   
   private
 
   def expire_cache_for(place)
-    Rails.logger.info("spot-app: expiring cache for Place #{place.id}")
+    Rails.logger.info("spot-app: expiring cache for place #{place.to_param}")
     expire_page(:controller=>"site/places",:action=>"show",:id => place.id)
     expire_page(:controller=>"site/places",:action=>"show",:id => place.to_param)
     expire_page(:controller=>"site/sitemaps", :action=>"show")
--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,7 @@ module Spot
     # Set the Console Logger to STDOUT
     config.logger = Logger.new(STDOUT) if defined? Rails::Console
     config.secret = "6f0b012c7d37f3357d137b30968bc67cf61a4bc4956ddbbf1896e9ff9dba5f7ef258705a6e894c9f39998360fe857dfba18e409b8b6f5ba2164f361fe948ca9d"  
+    config.active_record.observers = [:place_sweeper]
+    
   end
 end
