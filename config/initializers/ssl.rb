afbd309b6c7aa5baa0d2a5a9a40a4cd28c21473a
Jesse Reiss <jreiss@oogalabs.com>
2011-04-12 08:31:54 -0700

    missing hsts

--- config/initializers/ssl.rb
+++ config/initializers/ssl.rb
@@ -1,3 +1,3 @@
 if Rails.env.production?  
-  Rails.configuration.middleware.use Rack::SslEnforcer, { :expires => 500 }
+  Rails.configuration.middleware.use Rack::SslEnforcer, :hsts => { :expires => 500 }
 end
