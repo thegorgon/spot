c88d032ff8a2d4c427229fda4e4807c59e2a2937
Jesse Reiss <jreiss@oogalabs.com>
2011-04-11 17:25:10 -0700

    10000

--- app/controllers/site/sitemaps_controller.rb
+++ app/controllers/site/sitemaps_controller.rb
@@ -2,7 +2,7 @@ class Site::SitemapsController < Site::BaseController
   caches_page :show
 
   def show
-    @places = Place.order("id DESC").limit(1000).all
+    @places = Place.order("id DESC").limit(10000).all
     render :layout => false
   end
 end
