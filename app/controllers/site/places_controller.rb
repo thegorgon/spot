1714cc68396f9fe1dccd683c2eec3a719d23ca7e
Jesse Reiss <jreiss@oogalabs.com>
2011-04-11 20:54:10 -0700

    cacheing fix and log level

--- app/controllers/site/places_controller.rb
+++ app/controllers/site/places_controller.rb
@@ -1,6 +1,8 @@
 class Site::PlacesController < Site::BaseController
   layout 'emptysite'
-  caches_action :show
+  caches_action :show,
+    :cache_path => Proc.new { |c| [c.send(:place_path, c.params[:id]), c.send(:locale), REVISION].join('/').gsub(/^\//, '') },
+    :expires_in => 1.week
   
   def show
     @place = Place.find(params[:id])
