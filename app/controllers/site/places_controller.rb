9ec01c1b7ecd432a13c3eeb63e9944d138501973
Jesse Reiss <jreiss@oogalabs.com>
2011-04-15 08:23:58 -0700

    places

--- app/controllers/site/places_controller.rb
+++ app/controllers/site/places_controller.rb
@@ -2,7 +2,7 @@ class Site::PlacesController < Site::BaseController
   layout 'emptysite'
 
   caches_action :show,
-    :cache_path => Proc.new { |c| AppSetting.cache_path(:blog, c) },  
+    :cache_path => Proc.new { |c| AppSetting.cache_path(:place, c) },  
     :expires_in => 1.week
   
   def show
