86c300dceb24d536d2c36c5152773075fe050501
Jesse Reiss <jreiss@oogalabs.com>
2011-04-19 12:11:14 -0700

    damn i should've expected that

--- vendor/plugins/wrapr/lib/wrapr/request/cacheing.rb
+++ vendor/plugins/wrapr/lib/wrapr/request/cacheing.rb
@@ -4,7 +4,7 @@ module Wrapr
       def load_from_cache
         begin
           cache_value = Rails.cache.read(cache_string)
-          @response = cache_value[:response] if Time.now < cache_value[:expires]
+          @response = cache_value[:response] if cache_value && Time.now < cache_value[:expires]
         rescue ArgumentError => e
           raise unless e.to_s =~ /undefined class/ # unexpected error message, re-raise
           e.to_s.split.last.constantize            # raises NameError if it can't find the constant
