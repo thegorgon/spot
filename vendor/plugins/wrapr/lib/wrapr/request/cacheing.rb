119ab5949a768d4914286b887ba7166e03d5a629
Jesse Reiss <jreiss@oogalabs.com>
2011-04-19 12:02:20 -0700

    cache expiry

--- vendor/plugins/wrapr/lib/wrapr/request/cacheing.rb
+++ vendor/plugins/wrapr/lib/wrapr/request/cacheing.rb
@@ -3,7 +3,8 @@ module Wrapr
     module Cacheing
       def load_from_cache
         begin
-          @response = Rails.cache.read(cache_string)
+          cache_value = Rails.cache.read(cache_string)
+          @response = cache_value[:response] if Time.now < cache_value[:expires]
         rescue ArgumentError => e
           raise unless e.to_s =~ /undefined class/ # unexpected error message, re-raise
           e.to_s.split.last.constantize            # raises NameError if it can't find the constant
@@ -12,7 +13,7 @@ module Wrapr
       end
 
       def cache_string
-        string = "#{self.class.to_s.underscore}#{request_path}"
+        string = "#{self.class.to_s.underscore}/v1/#{request_path}"
         string << (string.index('?') ? "&#{@body}" : "?#{@body}") if @body.present?
         string
       end
