86bcb509ee4ccaeab68691b43a50a653c0ef3d8e
Jesse Reiss <jreiss@oogalabs.com>
2011-04-29 12:22:31 -0700

    capture failed saves

--- app/controllers/site/previews_controller.rb
+++ app/controllers/site/previews_controller.rb
@@ -1,8 +1,11 @@
 class Site::PreviewsController < Site::BaseController
   def create
     @preview = PreviewSignup.signup(params[:preview])
-    success = @preview.save
-    redirect_to preview_share_url(@preview.rid)
+    if @preview.save
+      redirect_to preview_share_path(@preview.rid)
+    else
+      redirect_to root_path
+    end
   end
   
   def share
