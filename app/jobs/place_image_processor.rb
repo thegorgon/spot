d6b2d7e2bb803eeb59245dea3d3dc3a0c2e94cb3
Jesse Reiss <jreiss@oogalabs.com>
2011-05-04 23:15:06 -0700

    try to fix this issue

--- app/jobs/place_image_processor.rb
+++ app/jobs/place_image_processor.rb
@@ -18,14 +18,14 @@ module Jobs
     def self.perform_file_download(instance, uri, attachment_name=:image)
       file = download_external_file(uri)
       instance.attachment_for(attachment_name).assign(file)
-      perform_image_processing(instance, attachment_name, file)
+      perform_image_processing(instance, attachment_name)
     end
     
-    def self.perform_image_processing(instance, attachment_name=:image, file=nil)
+    def self.perform_image_processing(instance, attachment_name=:image)
       instance.attachment_for(attachment_name).reprocess!
-      file ||= download_external_file(instance.attachment_for(attachment_name).url)
+      file = download_external_file(instance.attachment_for(attachment_name).url_without_processed(:original))
       lq_thumb = Paperclip.processor(:thumbnail).make(file, {:geometry => "117x84#", :convert_options => '-quality 10 -strip -colorspace RGB', :format => 'jp2'}, instance)
-      instance.image_thumbnail = ActiveSupport::Base64.encode64(lq_thumb.to_a.join)      
+      instance.image_thumbnail = ActiveSupport::Base64.encode64(lq_thumb.to_a.join)
       instance.image_processing = false
       instance.save!
     end
