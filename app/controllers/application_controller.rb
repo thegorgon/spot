9f7b4e2e2685ddcc8bbdb2c221f494662dda8152
Jesse Reiss <jreiss@oogalabs.com>
2011-05-05 11:28:12 -0700

    limit notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < ActionController::Base
     message << exception.annoted_source_code.to_s if exception.respond_to?(:annoted_source_code)
     message << "  " << clean_backtrace(exception, :silent).join("\n  ")
     Rails.logger.fatal("#{message}\n\n") if Rails.logger
-    notify_hoptoad(exception)
+    notify_hoptoad(exception) if exception.notifiable?
   end
 
   def render_error(exception)
--- lib/exceptions/exceptions.rb
+++ lib/exceptions/exceptions.rb
@@ -1,3 +1,15 @@
+module NotifiableError
+  def notifiable?
+    true
+  end
+end
+
+module NotNotifiableError
+  def notifiable?
+    false
+  end
+end
+
 class ServiceError < IOError; end
 
 class ExternalServiceError < ServiceError; end
@@ -5,3 +17,12 @@ class ExternalServiceError < ServiceError; end
 class UnauthorizedAccessError < IOError; end
 
 class DuplicateConstantError < StandardError; end
+
+Exception.send(:include, NotifiableError)
+
+[ UnauthorizedAccessError, 
+  ActiveRecord::RecordNotUnique, 
+  ActiveRecord::RecordInvalid, 
+  ActiveRecord::RecordNotFound ].each do |error|
+  error.send(:include, NotNotifiableError)
+end
