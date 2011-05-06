c0bea75981cdb5ce1fdce588225818024d6399f9
Jesse Reiss <jreiss@oogalabs.com>
2011-05-05 17:25:17 -0700

    blog fix

--- app/controllers/site/blog_controller.rb
+++ app/controllers/site/blog_controller.rb
@@ -1,8 +1,4 @@
-class Site::BlogController < Site::BaseController  
-  caches_action :index,
-    :cache_path => Proc.new { |c| AppSetting.cache_path(:blog, c) },
-    :expires_in => 1.week
-
+class Site::BlogController < Site::BaseController 
   def index
     @posts = Wrapr::Tumblr::Item.paginate(:page => params[:page], :per_page => params[:per_page])
   end
