52ef5cc65cb4b2e2eb9b18d461c0a34cd4317dfd
Jesse Reiss <jreiss@oogalabs.com>
2011-05-10 15:16:38 -0700

    ok, make sphinx speak canonical_id but search speak id

--- app/models/place.rb
+++ app/models/place.rb
@@ -58,6 +58,11 @@ class Place < ActiveRecord::Base
     finder
   end
   
+  def self.search(*args)
+    ids = search_for_ids(*args)
+    where(:id => ids).all
+  end
+  
   def self.dedupe!
     all.each { |p| DuplicatePlace.dedupe(p) }
   end
