8e9463cea65927249e04442b2cd65c04b483aef2
Jesse Reiss <jreiss@oogalabs.com>
2011-06-27 16:20:00 -0700

    gotta send daily emails

--- app/models/biz/business_account.rb
+++ app/models/biz/business_account.rb
@@ -39,6 +39,12 @@ class BusinessAccount < ActiveRecord::Base
     end
   end
   
+  def self.deliver_daily_notifications 
+    Business.find_each do |biz|
+      biz.deliver_promotion_codes_for!(Date.today)
+    end
+  end
+  
   def self.deliver_weekly_notifications
     with_setting("weekly_digest").find_each do |account|
       BusinessMailer.weekly_digest(account).deliver!
