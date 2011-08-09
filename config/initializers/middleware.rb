0c252d1287a4ceb4ce7898bd165529b8311cd1b4
Jesse Reiss <jreiss@oogalabs.com>
2011-08-09 12:48:42 -0700

    no ssl on blog

--- config/initializers/middleware.rb
+++ config/initializers/middleware.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?  
-  Rails.configuration.middleware.use Rack::SslEnforcer, :hsts => { :expires => 500 }
+  Rails.configuration.middleware.use Rack::SslEnforcer, :hsts => { :expires => 500 }, :except => /^\/blog\//, :strict => true
 end
 
 Rails.configuration.middleware.use Rack::NoIE, :redirect => "/upgrade", :minimum => 8.0
