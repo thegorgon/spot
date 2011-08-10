e6e0b0ef90ffe17455e8a319971340347281c995
Jesse Reiss <jreiss@oogalabs.com>
2011-08-09 22:55:36 -0700

    ignore forced ssl on api and better device management

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -7,15 +7,14 @@ class Api::BaseController < ApplicationController
   
   def device_specifications
     unless @device_specifications
-      #"Spot 1.2 rv:72 (iPhone Simulator; iPhone OS 5.0; en_US)"
-      /Spot (\d+)\.(\d+) rv:(\d+) \(([^\;]+); ([^\;]+); ([^\)]+)\)/.match(request.user_agent) do |match|
+      #"Spot 1.1.2 rv:72 (iPhone Simulator; iPhone OS 5.0; en_US)"
+      /Spot ([\d\.]+) rv:(\d+) \(([^\;]+); ([^\;]+); ([^\)]+)\)/.match(request.user_agent) do |match|
         @device_specifications = {
-          :major_revision => match[1].to_i,
-          :minor_revision => match[2].to_i,
-          :app_version => match[3].to_i,
-          :device_name => match[4].to_i,
-          :os_id => match[5].to_i,
-          :locale => match[6].to_i,
+          :revision => match[1].to_i,
+          :app_version => match[2].to_i,
+          :device_name => match[3].to_i,
+          :os_id => match[4].to_i,
+          :locale => match[5].to_i,
         }
       end
     end
