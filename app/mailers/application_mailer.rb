282db56e3fcec0438f1c359334426f2d0ed21482
Jesse Reiss <jreiss@oogalabs.com>
2011-08-24 15:57:08 -0700

    fix kludge and fix display bug in ie

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,15 +1,19 @@
 class ApplicationMailer < ActionMailer::Base
+  include AbstractController::Callbacks
   layout 'mailer'
+  before_filter :set_inline_attachments
   default_url_options[:host] = HOSTS[Rails.env]
   default :from => "The Spot Team <spot@spotmembers.com>",
           'List-Unsubscribe' => Proc.new {  "<#{email_url(:email => @email, :key => EmailSubscriptions.passkey(@email))}>" },
           :to => Proc.new { @email },
-          "X-Sent" => Proc.new { set_x_sent },
           :subject => Proc.new { @title }
   
-  def set_x_sent
+
+  private
+  
+  def set_inline_attachments
     attachments.inline["logo.png"] = File.read("#{Rails.root}/public/images/logos/shdwapp80x140.png")
     attachments.inline["footer.jpg"] = File.read("#{Rails.root}/public/images/backgrounds/emailft598x110.jpg")
-    Time.now.utc.to_s(:w3c)
   end
+  
 end
