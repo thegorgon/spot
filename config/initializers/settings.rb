1699ca6be70090c97732e6fb49ff4c87ec091249
Jesse Reiss <jreiss@oogalabs.com>
2011-09-01 10:31:06 -0700

    email config

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,6 +1,9 @@
+ActionMailer::Base.prepend_view_path File.join('app', 'views', 'mailers')
+
 Sass::Plugin.options[:style] = :compressed
 Sass::Plugin.options[:sass2] = true
 Sass::Plugin.options[:template_location] = File.join(Rails.root, 'app', 'sass')
+
 ActiveRecord::Base.include_root_in_json = false
 
 Google.options[:config_file] = File.join(Rails.root, 'config', 'apis', 'google.yml')
