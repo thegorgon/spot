64790ea0f408850885f3e610b4bba54c932458f5
Jesse Reiss <jreiss@oogalabs.com>
2011-09-02 15:54:08 -0700

    fix event tracking error

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,9 +1,9 @@
 class Api::EventsController < Api::BaseController
   def create
     if params[:type] == 'acquisition'
-      record_user_event(params[:event], params[:value])
-    else
       record_acquisition_event(params[:event], params[:value])
+    else
+      record_user_event(params[:event], params[:value])
     end
     head :ok
   end
