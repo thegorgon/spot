a557667800bc7e158d9bca719f5afcf7cd6f0a9b
Jesse Reiss <jreiss@oogalabs.com>
2011-09-02 10:03:17 -0700

    blitz email layout

--- app/mailers/blitz_mailer.rb
+++ app/mailers/blitz_mailer.rb
@@ -1,3 +1,26 @@
 class BlitzMailer < ApplicationMailer
   layout 'blitz'
+  
+  def email(recipient, day)
+    @email = recipient.email
+    @title = "Jump the Velvet Rope for $5 a Month"
+    if schedule[:quotes].include?(day)
+      attachments.inline["header.png"] = File.read(Rails.root.join("public", "images", "email", "blitz", "quoteheader.png"))
+      attachments.inline["quote.png"] = File.read(Rails.root.join("public", "images", "email", "blitz", "quote#{day}.png"))
+    else
+      attachments.inline["header.png"] = File.read(Rails.root.join("public", "images", "email", "blitz", "header#{day}.png"))      
+    end
+    mail(:template_name => "email#{day}")
+  end
+  
+  private
+  
+  def set_inline_attachments
+  end
+
+  def schedule
+    {
+      :quotes => [1,2,3]
+    }
+  end
 end
