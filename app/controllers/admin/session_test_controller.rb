7a890f9ca5c49638147ce35b2e542a850b1dd951
Jesse Reiss <jreiss@oogalabs.com>
2011-09-09 12:52:33 -0700

    always expiration date with redis, so important

--- app/controllers/admin/session_test_controller.rb
+++ app/controllers/admin/session_test_controller.rb
@@ -2,7 +2,7 @@ class Admin::SessionTestController < Admin::BaseController
   def new
     @value = Nonce.friendly_token
     session[:session_test_value] = @value
-    Rails.cache.write("session_test_value", @value, :expires => 1.day)
+    Rails.cache.write("session_test_value", @value, :expires_in => 1.day)
   end
   
   def show
