9ad975de9a453728b809c3037dbbc990e58abc54
Jesse Reiss <jreiss@oogalabs.com>
2011-09-12 12:46:25 -0700

    promo event type

--- app/models/biz/promotion_event.rb
+++ app/models/biz/promotion_event.rb
@@ -84,6 +84,7 @@ class PromotionEvent < ActiveRecord::Base
     options = args.extract_options!
     if options[:api]
       {
+        :_type => self.class.to_s,
         :place => place.as_json(:skip_thumbnail => true),
         :image_url_640x400 => place.image.url(:i640x400),
         :image_url_234x168 => place.image.url(:i234x168),
