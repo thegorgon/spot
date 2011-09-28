a354c6e677c1d7b4011b0432622e02ef54ba323e
Jesse Reiss <jreiss@oogalabs.com>
2011-09-28 16:19:08 -0700

    deprecation warnings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -112,13 +112,13 @@ class ApplicationController < ActionController::Base
  
   def redirect_to_full_site
     redirect_to request.protocol + request.host_with_port.gsub(/^m\./, 'www.') +
-                request.request_uri and return
+                request.fullpath and return
   end
  
   def redirect_to_mobile_if_applicable
     unless mobile_request? || cookies[:prefer_full_site] || !mobile_browser?
       redirect_to request.protocol + "m." + request.host_with_port.gsub(/^www\./, '') +
-                  request.request_uri and return
+                  request.fullpath and return
     end
   end
  
